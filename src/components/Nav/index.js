e4b2a9dbba77ce208025cf45735479ac9d19c9f9
Khargol <josephklein3@gmail.com>
2020-10-12 17:35:07 -0400

    hooks for nav bar

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,24 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers'
 
 
-function Nav() {
-    const categories = [
-        {
-            name: "commercial",
-            description:
-                "Photos of grocery stores, food trucks, and other commercial projects",
-        },
-        { name: "portraits", description: "Portraits of people in my life" },
-        { name: "food", description: "Delicious delicacies" },
-        {
-            name: "landscape",
-            description: "Fields, farmhouses, waterfalls, and the beauty of nature",
-        },
-    ];
+function Nav(props) {
+    const {
+        categories = [],
+        setCurrentCategory,
+        currentCategory,
+      } = props;
 
-    function categorySelected(name){
-        console.log(`${name} clicked`);
-    }
+      useEffect(() => {
+        document.title = capitalizeFirstLetter(currentCategory.name);
+      }, [currentCategory]);
 
     return (
         <header>
@@ -39,11 +32,13 @@ function Nav() {
                     </li>
                     {categories.map((category) => (
                         <li
-                            className="mx-1"
+                            className={`mx-a ${
+                                currentCategory.name === category.name && 'navActive'
+                            }`}
                             key={category.name}
                         >
-                            <span onClick={() => categorySelected(category.name)}>
-                                {category.name}
+                            <span onClick={() => setCurrentCategory(category)}>
+                                {capitalizeFirstLetter(category.name)}
                             </span>
                         </li>
                     ))}
